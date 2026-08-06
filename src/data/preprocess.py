48312a3445bd52b463c380b1423f87add6360c0c
Le Van Tuan Em <levantuanem00@gmail.com>
2026-08-06 14:14:13 +0700

    Preprocessing data

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -2,9 +2,14 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 import joblib
+from sklearn import preprocessing
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
+import sklearn.preprocessing as StandardScaler
 
 data = pd.read_csv(r'D:\AI_Projects\ML_StudentScore_Projects\data\raw\StudentsPerformance.csv')
-print(data.head())
+print(data.info())
+print("Original lenght of Dataframe: {}".format(len(data)))
+data.dropna(inplace= True)
+print("Lenght of Dataframe afer removing NAN values: {}".format(len(data)))
