3f85d777746faf6130a2c602f610332ed198c3a0
Le Van Tuan Em <levantuanem00@gmail.com>
2026-08-06 11:01:46 +0700

    Init src/data

---
+++ README.md
@@ -0,0 +1,1 @@
+"# ML_StudentScore_Projects" 
---
+++ src/data/processing.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import logging
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+
+data = pd.read_csv(r'D:\AI_Projects\ML_StudentScore_Projects\data\raw\StudentsPerformance.csv')
+print(data.head())
